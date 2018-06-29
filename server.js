fe04dfdce3b37d503cf9e854123477abff6eb07e
Fernando Rogelin <fernando.filho@ilegra.com>
2018-06-29 15:04:47 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,17 @@
+let express = require('express')
+let app = express();
+let port = 3000;
+
+app.get('/v1/restaurants', (rep, res) => {
+    // list mongo
+    const response = [
+        { nome: 'Pampa', endereço: 'Av. Goethe, 13 - Moinhos de Vento', valor: 'R$19,90' },
+        { nome: 'Delícia', endereço: 'Av. Carlos Gomes, 249 - Auxiliadora', valor: 'R$22,00' }
+    ];
+    res.send({ data: response });
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log('Para derrubar o servidor: crtl + c');
+})
