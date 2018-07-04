1c19feeb714b045010bf686a9ec10157842b217c
Fernando Rogelin <fernando.filho@ilegra.com>
2018-07-04 15:01:56 -0300

    Test

--- server.js
+++ server.js
@@ -6,13 +6,18 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 // Schema de validação
-// const Restaurants = require("./Models/restaurants");
+const Restaurants = require("./Models/restaurants");
 
 // Porta onde irei conectar
 const port = process.env.PORT;
 
 // Conecção com o mLab
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true
+  }
+);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json);
